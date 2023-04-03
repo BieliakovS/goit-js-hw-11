474a906777666ddf6a648edcb3d7c64c75d3ecd1
BieliakovS <93442308+BieliakovS@users.noreply.github.com>
2023-04-04 00:40:36 +0300

    2

--- src/index.js
+++ src/index.js
@@ -5,27 +5,35 @@ import Notiflix from 'notiflix';
 const refs = {
   searchForm: document.querySelector('#search-form'),
   gallery: document.querySelector('.gallery'),
+  loadMoreBtn: document.querySelector('.load-more'),
 };
 
-function onSubmitForm(e) {
+refs.searchForm.addEventListener('submit', onSubmitForm);
+
+async function onSubmitForm(e) {
   e.preventDefault();
   const inputValue = refs.searchForm.elements['searchQuery'].value;
 
-  getImages(inputValue)
-    .then(({ hits }) => {
-      if (hits.length === 0) {
-        Notiflix.Notify.failure(
-          'Sorry, there are no images matching your search query. Please try again.'
-        );
-      }
-      refs.gallery.innerHTML = '';
-      let markup = '';
-      for (let i = 0; i < hits.length; i += 1) {
-        markup += ImageCard(hits[i]);
-      }
-      refs.gallery.innerHTML = markup;
-    })
-    .catch(console.warn);
+  try {
+    const obj = await getImages(inputValue);
+
+    if (obj.data.hits.length === 0) {
+      Notiflix.Notify.failure(
+        'Sorry, there are no images matching your search query. Please try again.'
+      );
+    } else if (obj.data.hits.length < 40) {
+      markGallery(obj);
+    }
+    markGallery(obj);
+  } catch (error) {
+    console.warn(error);
+  }
 }
 
-refs.searchForm.addEventListener('submit', onSubmitForm);
+function markGallery(obj) {
+  let markup = '';
+  for (let i = 0; i < obj.data.hits.length; i += 1) {
+    markup += ImageCard(obj.data.hits[i]);
+  }
+  refs.gallery.innerHTML = markup;
+}
--- src/js/getImages.js
+++ src/js/getImages.js
@@ -1,34 +1,18 @@
 const API_KEY = '34987662-26e97d4e150e3c854c752264a';
 const BASE_URL = 'https://pixabay.com/api/';
 
-// import axios from 'axios';
+import axios from 'axios';
 
-// export const getImages = value => {
-//   return axios.get(`${BASE_URL}`, {
-//     params: {
-//       key: API_KEY,
-//       q: value,
-//       image_type: 'photo',
-//       orientation: 'horizontal',
-//       safesearch: true,
-//     },
-//   });
-// };
-
-
-
-export const getImages = value =>
-  fetch(
-    `${BASE_URL}/?key=${API_KEY}&q=${value}&image_type=photo&orientation=horizontal&safesearch=true&page=1&per_page=40`
-  )
-    .then(response => {
-      if (!response.ok) {
-        throw new Error(`HTTP error! status: ${response.status}`);
-      }
-      return response.json();
-    })
-    .catch(error => {
-      console.warn(error);
-      return null;
-    });
-  
+export const getImages = value => {
+  return axios.get(`${BASE_URL}`, {
+    params: {
+      key: API_KEY,
+      q: value,
+      image_type: 'photo',
+      orientation: 'horizontal',
+      safesearch: true,
+      per_page: 40,
+      page: 1,
+    },
+  });
+};
