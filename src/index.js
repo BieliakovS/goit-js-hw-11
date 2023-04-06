e171dabc337e1801e75dbc48139b2a6375981b0a
BieliakovS <93442308+BieliakovS@users.noreply.github.com>
2023-04-06 21:38:03 +0300

    autoScroll

--- src/index.js
+++ src/index.js
@@ -90,4 +90,15 @@ function markGallery(obj) {
   const lightbox = new SimpleLightbox('.gallery a', {});
 
   lightbox.refresh();
+
+  if (nowHits > obj.config.params.per_page) {
+    const { height: cardHeight } = document
+      .querySelector('.gallery')
+      .firstElementChild.getBoundingClientRect();
+
+    window.scrollBy({
+      top: cardHeight * 2,
+      behavior: 'smooth',
+    });
+  }
 }
